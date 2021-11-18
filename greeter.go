f1de548b3f14dec53cca15bd297dc253f5551826
Teymur Gahramanov <36300147+teymurgahramanov@users.noreply.github.com>
2021-11-18 09:59:25 +0400

    Added hostname to output

--- greeter.go
+++ greeter.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
+
+func Greet(w http.ResponseWriter, r *http.Request) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Fprintf(w, "Hi! I am on %s", hostname)
+}
 
 func main() {
 	http.HandleFunc("/", Greet)
 	http.ListenAndServe(":8080", nil)
-}
-
-func Greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi!")
 }
